d6c75a4db57dbd84b94678b86c9aac20ae012b65
Elliott Brown <64017606@ep-student.org>
2022-02-15 13:55:28 -0600

    H

---
+++ testproject/testproject/testprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testprojectApp.swift
+//  testproject
+//
+//  Created by 64017606 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct testprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
